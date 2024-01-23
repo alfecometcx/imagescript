0fdfc5f4fa1b164c9f3f45d4b350844a4338a62d
alfecaesar <alfe@comet.cx>
2024-01-24 07:20:09 +0800

    new updates

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = 3000;
 
 app.get('/image.png', async (req, res) => {
   try {
-    // Replace these with the actual URLs of your 4 different images
     const imageSources = [
       'https://2.img-dpreview.com/files/p/E~C1000x0S4000x4000T1200x1200~articles/3925134721/0266554465.jpeg',
       'https://fastly.picsum.photos/id/15/2500/1667.jpg?hmac=Lv03D1Y3AsZ9L2tMMC1KQZekBVaQSDc1waqJ54IHvo4',
@@ -16,7 +15,6 @@ app.get('/image.png', async (req, res) => {
       'https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA',
     ];
 
-    // Download images from URLs
     const imageBuffers = await Promise.all(
       imageSources.map(async (url) => {
         const response = await axios.get(url, { responseType: 'arraybuffer' });
@@ -24,7 +22,6 @@ app.get('/image.png', async (req, res) => {
       })
     );
 
-    // Create a 2x2 grid using sharp
     const images = await Promise.all(
       imageBuffers.map(async (buffer) => await sharp(buffer).resize(200, 200).toBuffer())
     );
@@ -39,17 +36,16 @@ app.get('/image.png', async (req, res) => {
     })
       .composite([
         { input: images[0], top: 0, left: 0 },
-        { input: images[1], top: 0, left: 202 },
-        { input: images[2], top: 202, left: 0 },
-        { input: images[3], top: 202, left: 202 },
+        { input: images[1], top: 0, left: 200 },
+        { input: images[2], top: 200, left: 0 },
+        { input: images[3], top: 200, left: 200 },
       ])
       .png()
       .toBuffer();
 
-    // Save the result image locally (optional)
-    fs.writeFileSync('download-img.png', resultImage);
+    // download image
+    // fs.writeFileSync('download-img.png', resultImage);
 
-    // Send the result image as a response
     res.type('png').send(resultImage);
   } catch (error) {
     console.error(error);
